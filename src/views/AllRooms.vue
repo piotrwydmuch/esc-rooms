b3aad643d5daa21cef2c935f6866e220e6b44c63
piotr <piotrwydmuch95@gmail.com>
2024-09-29 21:46:14 +0200

    panel template for saving changes done

--- src/views/AllRooms.vue
+++ src/views/AllRooms.vue
@@ -23,11 +23,15 @@ onMounted(() => {
     :title="'Wszystkie pokoje'"
     :subtitle="'Przeglądaj Escape Roomy. Oznacz pokoje, które odwiedziłeś.'"
   />
-  <div id="all-rooms" class="container mx-auto px-4 py-8">
+  <div id="all-rooms" class="container mx-auto px-4 py-8 relative">
     <h2 class="text-2xl font-bold mb-6 text-gray-800">All Escape Rooms</h2>
     <ul class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-1 gap-6">
       <Room v-for="room in escapeRooms" :key="room.id" :room="room" class="bg-white rounded-lg shadow-md overflow-hidden transition-transform hover:scale-105" />
     </ul>
+    <div class="right-0 flex justify-between fixed bottom-0 gap-4 p-4 bg-white shadow-md">
+      <button class="px-4 py-2 bg-indigo-500 shadow-md text-white">Zapisz zmiany</button>
+      <button class="px-4 py-2 bg-white shadow-md">Anuluj</button>
+    </div>
   </div>
 </template>
 
